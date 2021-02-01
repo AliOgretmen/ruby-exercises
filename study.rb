309d95898aee1fcb63b6d7c68ca76f5a5ac4dcaf
AliOgretmen <aliogretmen.ch@gmail.com>
2021-02-01 22:04:15 +0100

    test commit

--- study.rb
+++ study.rb
@@ -11,3 +11,20 @@ puts myVar.eql?(compareVar)
 
 # if Statements
 
+num = 8
+if num == 3
+    puts "Number is 3"
+elsif num == 10
+    puts "Number is 10"
+elsif num == 7
+    puts "Number is 7"
+else
+    puts "Not found"
+end
+
+a = 42
+unless a < 10
+    puts "Yes"
+else
+    puts "No"
+end
