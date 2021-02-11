cafc1d47afd960003d2eb47cabee3725f749d567
AliOgretmen <aliogretmen.ch@gmail.com>
2021-02-11 22:37:05 +0100

    self

--- study.rb
+++ study.rb
@@ -343,7 +343,24 @@ class Person
         @name = name
     end
 end
-
 p = Person.new("David")
 p.name = "Bob"
 puts p.name
+#Accessor Methods - self
+class Person
+    attr_accessor :name, :age
+    def initialize(name, age)
+        @name = name
+        @age = age
+    end
+    def change(n, a)
+        self.name = n
+        self.age = a
+    end
+    def show_info
+        puts "#{self.name} is #{self.age}"
+    end
+end
+p = Person.new("David", 28)
+p.change("Bob", 42)
+p.show_info
