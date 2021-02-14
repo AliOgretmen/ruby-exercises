1dd7d45125493ead85650c1d55d68386d21e02aa
AliOgretmen <aliogretmen.ch@gmail.com>
2021-02-14 22:44:31 +0100

    overloading

--- study.rb
+++ study.rb
@@ -473,3 +473,20 @@ class Cat  < Animal
 end
 c = Cat.new("Bob", 3)
 puts c
+
+#Operator Overloading
+class Shape
+    attr_accessor :h, :w
+    def initialize(h, w)
+        self.h = h
+        self.w = w
+    end
+    def +(other)
+        Shape.new(self.h+other.h, self.w+other.w)
+    end
+end
+a = Shape.new(7, 4)
+b = Shape.new(9, 18)
+c = a+b
+puts c.h
+puts c.w 
