b247a447f75b48b7e8e731f5da28fb3b83be374c
AliOgretmen <aliogretmen.ch@gmail.com>
2021-02-15 23:34:27 +0100

    Access Modifiers

--- study.rb
+++ study.rb
@@ -488,5 +488,41 @@ end
 a = Shape.new(7, 4)
 b = Shape.new(9, 18)
 c = a+b
-puts c.h
-puts c.w 
+puts c.h  # 16
+puts c.w  # 22
+
+# Access Modifiers
+class Person
+    def initialize(age)
+        @age = age
+    end
+    def show
+        puts "#{@age} years = #{days_lived} days"
+    end
+
+    private  #private
+    def days_lived
+        @age * 365
+    end
+end
+p = Person.new(42)
+p.show  # 42 years = 15330 days
+#-----------
+#Protected
+class Product
+    attr_accessor :name, :num
+    def initialize(name, num)
+        @name = name
+        @num = num
+    end
+    def ==(other)
+        self.id == other.id
+    end
+    protected
+    def id
+        name.length*num
+    end
+end
+p1 = Product.new("PC", 5)
+p2 = Product.new("Laptop", 3)
+puts (p1 == p2) # false
